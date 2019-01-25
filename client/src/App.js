578529cbcfdcf4222dfd9628b650c63a6fe488e8
josepheastman <josephryan96@gmail.com>
2019-01-25 10:19:32 -0800

    Jokes showing on client when logged in

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,19 @@ import styled from "styled-components";
 
 import Signup from "./components/Signup.js";
 import Signin from "./components/Signin.js";
-// import Jokes from "./components/Jokes.js";
+import Jokes from "./components/Jokes.js";
+
+const StyledNav = styled.div`
+  // display: flex;
+  // flex-direciton: row;
+  // margin-left: auto;
+  // margin-right: auto;
+  // display: inline-block;
+  // height: 100%;
+  margin-left: 500px;
+  font-family: cursive;
+  font-size: 20px;
+`;
 
 class App extends Component {
   render() {
@@ -14,19 +26,59 @@ class App extends Component {
       <div>
         <div>
           <header>
-            <nav>
-              <NavLink to="/signup">Sign up</NavLink>
-              <NavLink to="/signin">Sign in</NavLink>
-              &nbsp;|&nbsp;
-              <NavLink to="/jokes">Jokes</NavLink>
+            <StyledNav>
+              <NavLink
+                to="/signup"
+                style={{
+                  fontWeight: "bold",
+                  color: "black",
+                  textDecoration: "none",
+                  marginRight: "300px"
+                }}
+              >
+                Sign up
+              </NavLink>
+              <NavLink
+                to="/signin"
+                style={{
+                  fontWeight: "bold",
+                  color: "black",
+                  textDecoration: "none",
+                  marginRight: "300px"
+                }}
+              >
+                Sign in
+              </NavLink>
+              <NavLink
+                to="/jokes"
+                style={{
+                  fontWeight: "bold",
+                  color: "black",
+                  textDecoration: "none",
+                  marginRight: "20px",
+                  marginRight: "300px"
+                }}
+              >
+                Jokes
+              </NavLink>
               <button onClick={this.signout}>
-                <NavLink to="/signin">Sign out</NavLink>
+                <NavLink
+                  to="/signin"
+                  style={{
+                    fontWeight: "bold",
+                    color: "black",
+                    textDecoration: "none"
+                  }}
+                >
+                  Sign out
+                </NavLink>
               </button>
-            </nav>
+            </StyledNav>
           </header>
           <main>
             <Route path="/signup" component={Signup} />
             <Route path="/signin" component={Signin} />
+            <Route path="/jokes" component={Jokes} />
           </main>
         </div>
       </div>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import styled from "styled-components";
 
 class Signup extends React.Component {
   state = {
